0af40b90ce3c66be4bb24bcd341eeff314afcf70
Willem Jan Faber <willemjan.faber@fe2.nl>
2016-09-08 10:33:04 +0200

    meertens firstnames

---
+++ pp/lang/nl/meertens_firstnames.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+"""
+    narralyzer_languagemodel.pp.lang.nl.meertens_firstnames
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Simple harvester for Dutch first names, and their freq.
+
+    Data-source:
+        http://www.meertens.knaw.nl/nvb/
+
+    :copyright: (c) 2016 Koninklijke Bibliotheek, by Willem-Jan Faber.
+    :license: GPLv3
+    https://github.com/KBNLresearch/Narralyzer/blob/master/licence.txt
+"""
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+import json
+import logging
+import os
+import pickle
+import random
+import string
+import urllib
+
+from lxml import etree
+from pprint import pprint
+from StringIO import StringIO
+
+
+class MeertensFirstnameHarvester:
+    BASEURL = 'http://www.meertens.knaw.nl/nvb/naam/bevat/%s'
+    BASEURL1 = 'http://www.meertens.knaw.nl/nvb/naam/pagina%i/bevat/%s'
+
+    CACHE_F = 'first_names_f.pickle'
+    CACHE_M = 'first_names_m.pickle'
+
+    female = {}
+    male = {}
+
+    # http://www.phonetics.ucla.edu/course/chapter1/vowels.html
+    vowels = [
+            'a',
+            'e',
+            'i',
+            'o',
+            'oe',
+    ]
+
+    def __init__(self):
+        for k in self.vowels:
+            url = self.BASEURL % k
+            self._fetch_page(url)
+
+            log.debug('Writing %i first names to cache %s' % (
+                len(self.female.keys()) + len(self.male.keys()),
+                self.CACHE_M)
+            )
+
+            with open(self.CACHE_M, 'wb') as fh:
+                pickle.dump(self.female, fh)
+                
+            with open(self.CACHE_F, 'wb') as fh:
+                pickle.dump(self.male, fh)
+
+            for i in range(26665):
+                url = self.BASEURL1 % (i, k)
+
+                if not self._fetch_page(url):
+                    break
+
+                log.debug('Writing %i first names to cache %s' % (
+                    len(self.female.keys()) + len(self.male.keys()),
+                    self.CACHE_M)
+                )
+
+                print('F', k, sorted(self.female.keys())[-10:])
+                print('M', k, sorted(self.male.keys())[-10:])
+
+                with open(self.CACHE_F, 'wb') as fh:
+                    pickle.dump(self.female, fh)
+
+                with open(self.CACHE_M, 'wb') as fh:
+                    pickle.dump(self.male, fh)
+
+    def _fetch_page(self, url):
+        try:
+            html = urllib.urlopen(url).read()
+            log.debug('Reading %s' % url)
+            parser = etree.HTMLParser()
+            tree = etree.parse(StringIO(html), parser)
+            self._parse_tds(tree.xpath('//td'))
+            return True
+        except:
+            return False
+
+    def _parse_tds(self, tds):
+        for i in range(1, 16):
+            nr = i * 3
+
+            name = tds[nr].xpath('a')[0]
+            name = name.text.encode('utf-8')
+            name = name.replace('(', '').replace(')', '')
+
+            female = tds[nr + 2].text.split(' ')[-1]
+            male = tds[nr + 1].text.split(' ')[-1]
+
+            if not name in [self.female.keys() + self.male.keys()]:
+                if female == '-':
+                    female = 0
+                if male == '-':
+                    male = 0
+
+                if int(female) > int(male) and int(female) > 4:
+                    self.female[name] = female
+                elif int(female) > 1:
+                    self.female[name] = female
+                else:
+                    if int(male) > int(female) and int(male) > 4:
+                        self.male[name] = male
+                    elif int(male) > 1:
+                        self.male[name] = male
+
+logging.basicConfig()
+log = logging.getLogger("dutchnames")
+log.setLevel(logging.DEBUG)
+meertens = MeertensFirstnameHarvester()
---
+++ stanford/generate_stanford_lang_snapshot.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+
+
+# Goal of this install procedure is to get && and freeze the stanford-core (ner) jar's
+# Also fetch and free the language models. This enables the framework to keep track of
+# data and software versions used during classification.
+
+# This file is part of the Narralyzer project.
+
+
+# Narralyzer config util,
+# all (global) variables should be defined in the conf/conf.ini file.
+CONFIG=$(./narralyzer/config.py self)
+
+
+# Fetch the given URL, and save to disk
+# use the 'basename' for storing,
+# retry a couple of times before failing.
+function get_if_not_there () {
+    URL=$1
+    retries=4
+    not_done="true"
+    if [ ! -f $(basename $URL) ]; then
+        while [ $not_done == "true" ]; do
+           inform_user "Fetching $URL..."
+           wget_output=$(wget -q "$URL")
+           if [ $? -ne 0 ]; then
+               # If downloading fails, try again.
+               retries=$(($retries - 1))
+               if [ $retries == 0 ]; then
+                   $(wget -q "$URL")
+                   airbag "Error while fetching $URL, no retries left." $LINENO
+               else
+                   inform_user "Error while fetching $URL, $retries left." $LINENO
+                   sleep 1
+               fi
+           else
+               # Else leave the loop.
+               not_done="false"
+           fi
+        done
+    else
+        inform_user "Not fetching $URL, file allready there."
+    fi
+}
+
+
+# Fetch and unpack the Stanford core package.
+function fetch_stanford_core {
+    STANFORD_CORE=$($CONFIG stanford_core_source)
+    get_if_not_there $STANFORD_CORE
+    if [ -f $(basename $STANFORD_CORE) ]; then
+        unzip -q -n $(basename "$STANFORD_CORE")
+        # Remove the download package aferwards.
+        rm $(basename "$STANFORD_CORE")
+        # TODO: fix next line
+        ln -s $(find -name \*full\* -type d) core
+    fi
+}
+
+# Moves the retrieved classifiers into there respective lang dir, 
+# and generate md5sum usefull for reference later.
+function move_classifiers_inplace {
+    for lang in $($CONFIG supported_languages | xargs); do
+        target_path=$($CONFIG root)
+        target_path="$target_path"/"$($CONFIG stanford_ner_path)"/"$lang"
+        echo "target_path: $target_path"
+        if [ ! -d $target_path ]; then
+            mkdir -p $target_path || airbag "Could not create directory: $target_path" $LINENO
+            inform_user "Created directory: $target_path"
+        fi
+        src="$($CONFIG root)"/"$(find stanford/models -name $($CONFIG "lang_"$lang"_stanford_ner") -type f || airbag "Could not find model for $lang." $LINENO)"
+        checksum=$(md5sum -b "$src" | cut -d ' ' -f 1 || airbag "Failed to md5sum $src" $LINENO)
+        target="$target_path"/"$checksum"".crf.ser.gz"
+        inform_user "Moving classifier $src to $target."
+        # SHOWER-THOUGHT: I could also link them, and delete unused files..
+        # For now, this feels right.
+        mv "$src" "$target" || airbag "Failed to move $src to $target" $LINENO
+    done
+}
+
+
+# Fetch and unpack the language models.
+function fetch_stanford_lang_models {
+    for lang in $($CONFIG supported_languages | xargs);do
+        get_if_not_there $($CONFIG "lang_"$lang"_stanford_ner_source")
+    done
+    find . -name \*.jar -exec unzip -q -o '{}' ';'
+}
+
+# Check if Python2.7 is installed on the os,
+# we might need that in the near future.
+is_python2_7_avail() {
+    is_avail=$(which python2.7 | wc -l)
+    if [ "$is_avail" = "0" ]; then
+        airbag "Python 2.7 is not available, helas. sudo apt-get install python2.7?" $LINENO
+    fi
+    inform_user "Python 2.7 is available."
+}
+
+# Check if we find (Python) virtualenv.
+is_virtualenv_avail() {
+    is_avail=$(which virtualenv | wc -l)
+    if [ "$is_avail" = "0" ]; then
+        airbag "Virtualenv is not available, helas. sudo-apt-get install virtualenv?" $LINENO
+    fi
+    inform_user "Virtualenv is available."
+}
+
+# Fetch and unpack the language models.
+function fetch_stanford_lang_models {
+    for lang in $($CONFIG supported_languages | xargs);do
+        get_if_not_there $($CONFIG "lang_"$lang"_stanford_ner_source")
+    done
+    find . -name \*.jar -exec unzip -q -o '{}' ';'
+}
+
+# Check if Python2.7 is installed on the os,
+# we might need that in the near future.
+is_python2_7_avail() {
+    is_avail=$(which python2.7 | wc -l)
+    if [ "$is_avail" = "0" ]; then
+        airbag "Python 2.7 is not available, helas. sudo apt-get install python2.7?" $LINENO
+    fi
+    inform_user "Python 2.7 is available."
+}
+
+# Check if we find (Python) virtualenv.
+is_virtualenv_avail() {
+    is_avail=$(which virtualenv | wc -l)
+    if [ "$is_avail" = "0" ]; then
+        airbag "Virtualenv is not available, helas. sudo-apt-get install virtualenv?" $LINENO
+    fi
+    inform_user "Virtualenv is available."
+}
+
+
